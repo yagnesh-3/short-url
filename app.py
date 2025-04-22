d7cc5f63c4317a0fe7e506c1cff050f8dbdd85b2
Yagnesh <yagnesh.glk@gmail.com>
2025-04-22 14:24:32 +0530

    update

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ def redirect_url(short_url):
 if __name__ == "__main__":
     with open("urls.json","r") as f:
         urls= json.load(f)
-    app.run(host="0.0.0.0", port=5080)
+    app.run( port=5080)
 
